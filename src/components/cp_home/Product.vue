e99104138a4ca486405f2a38e9f48b8cba98bfc1
hungnt <trunghung5055@gmail.com>
2023-08-29 13:35:14 +0700

    fix js

--- src/components/cp_home/Product.vue
+++ src/components/cp_home/Product.vue
@@ -2,24 +2,26 @@
 export default {
   methods: {
     playOwlCarousel() {
-      $(".product-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-          '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-          '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-          0:{ items:1 },
-          576:{ items:2 },
-          768:{ items:3 },
-          992:{ items:4 }
-        }
-    });
+      (function ($) {
+        $(".product-carousel").owlCarousel({
+          autoplay: false,
+          smartSpeed: 1500,
+          margin: 30,
+          dots: false,
+          loop: true,
+          nav : true,
+          navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+          ],
+          responsive: {
+            0:{ items:1 },
+            576:{ items:2 },
+            768:{ items:3 },
+            992:{ items:4 }
+          }
+        });
+      })(jQuery);
     }
   },
   mounted() {
