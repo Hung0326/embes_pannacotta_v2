2be789993c17fa0d451a9e452d0ce156869630fa
hungnt <trunghung5055@gmail.com>
2023-08-29 13:50:47 +0700

    test

--- src/components/cp_home/Product.vue
+++ src/components/cp_home/Product.vue
@@ -2,26 +2,26 @@
 export default {
   methods: {
     playOwlCarousel() {
-      (function ($) {
-        $(".product-carousel").owlCarousel({
-          autoplay: false,
-          smartSpeed: 1500,
-          margin: 30,
-          dots: false,
-          loop: true,
-          nav : true,
-          navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-          ],
-          responsive: {
-            0:{ items:1 },
-            576:{ items:2 },
-            768:{ items:3 },
-            992:{ items:4 }
-          }
-        });
-      })(jQuery);
+      // (function ($) {
+      //   $(".product-carousel").owlCarousel({
+      //     autoplay: false,
+      //     smartSpeed: 1500,
+      //     margin: 30,
+      //     dots: false,
+      //     loop: true,
+      //     nav : true,
+      //     navText : [
+      //       '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      //       '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+      //     ],
+      //     responsive: {
+      //       0:{ items:1 },
+      //       576:{ items:2 },
+      //       768:{ items:3 },
+      //       992:{ items:4 }
+      //     }
+      //   });
+      // })(jQuery);
     }
   },
   mounted() {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 import '../node_modules/owl.carousel/dist/assets/owl.carousel.min.css'
 
 import '../node_modules/jquery/dist/jquery.min.js'
+import $ from 'jquery'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -10,6 +11,28 @@ import router from './router'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../node_modules/owl.carousel/dist/owl.carousel.min.js'
 
+$(document).ready(function($) {
+  (function ($) {
+    $(".product-carousel").owlCarousel({
+      autoplay: false,
+      smartSpeed: 1500,
+      margin: 30,
+      dots: false,
+      loop: true,
+      nav : true,
+      navText : [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+      ],
+      responsive: {
+        0:{ items:1 },
+        576:{ items:2 },
+        768:{ items:3 },
+        992:{ items:4 }
+      }
+    });
+  })(jQuery);
+})
 const app = createApp(App)
 
 app.use(router)
