c4114eb7fb3d2b79f147f28a2d3210fe9d118459
hungnt <trunghung5055@gmail.com>
2023-08-29 14:24:44 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import './assets/main.css'
-import $ from 'jquery'
-window.$ = $;
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -10,28 +8,6 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'owl.carousel';
 import '../node_modules/owl.carousel/dist/assets/owl.carousel.min.css'
 
-$(document).ready(function($) {
-  (function ($) {
-    $(".product-carousel").owlCarousel({
-      autoplay: false,
-      smartSpeed: 1500,
-      margin: 30,
-      dots: false,
-      loop: true,
-      nav : true,
-      navText : [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-      ],
-      responsive: {
-        0:{ items:1 },
-        576:{ items:2 },
-        768:{ items:3 },
-        992:{ items:4 }
-      }
-    });
-  })(jQuery);
-})
 const app = createApp(App)
 
 app.use(router)
